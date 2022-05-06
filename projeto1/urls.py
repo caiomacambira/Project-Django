0c5588dfd034f99f5ddacb305a499d527da8495c
Caio Macambira <caio_macambira@hotmail.com>
2022-05-06 14:37:17 -0300

    Criado novas URLS,deletado um aquivo .py de exemplo e criado a App receitas

--- projeto1/urls.py
+++ projeto1/urls.py
@@ -14,8 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    return HttpResponse('HOME')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATOS')
+
+
+def my_view(request):  # HTTP request
+    ...
+    return HttpResponse('Primeira URL')
+    # HTTP response
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', home),  # home
+    path('sobre/', sobre),  # /sobre
+    path('contato/', contato),  # /contato
+
+
 ]
