d30902a1f46f85ec81b7a4e3d108ea446c347081
Caio Macambira <caio_macambira@hotmail.com>
2022-05-18 10:15:31 -0300

    Template da pagina

--- receitas/views.py
+++ receitas/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     # rendezira o arquivo home.html
-    return render(request, 'receitas/home.html', context={
+    return render(request, 'receitas/pages/home.html', context={
         'name': 'Caio Macambira'
     })
