56ada010b87222aa8e924fb6ebf37b94f0b48c0f
Caio Macambira <caio_macambira@hotmail.com>
2022-05-19 14:44:13 -0300

    Finalizando templates

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views  # ( . ) da pasta que estou import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # home
-    path('receitas/<int:id>/', views.recipe),  # /recipe
+    path('', views.home, name="home"),  # home
+    path('receitas/<int:id>/', views.recipe, name="recipe"),  # /recipe
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,20 @@
 
+
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     # rendezira o arquivo home.html
     return render(request, 'receitas/pages/home.html', context={
-        'name': 'Caio Macambira'
+        # list comprehension de 10 receitas
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     # rendezira o arquivo home.html
     return render(request, 'receitas/pages/recipe-view.html', context={
-        'name': 'Caio Macambira'
+        'recipe': make_recipe(),
+        'is_detail_page': True,  # Se detail page não for True - mostre o botão
     })
